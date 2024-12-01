29b7463a49430cffa9a7697a9c4c8a469c7887fa
Michael Bui <michael.bui_crystal@outlook.com>
2024-12-02 02:00:02 +1300

    init sdl and debug rendering

--- main.cpp
+++ main.cpp
@@ -1,6 +1,48 @@
+#include <SDL2/SDL.h>
 #include <iostream>
 
+using namespace std;
+
+const int SCREEN_WIDTH = 600;
+const int SCREEN_HEIGHT = 800;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    // Initialise SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
+    }
+
+    // Create the window
+    SDL_Window *window = SDL_CreateWindow("Tetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH,
+                                          SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window) {
+        cerr << "Window could not be created!" << SDL_GetError() << endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    // Create a renderer
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    // The main game loop
+    bool isRunning = true;
+    SDL_Event event;
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                isRunning = false;
+            }
+        }
+
+        // Clear Screen
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Debug Render
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_Rect rect = {100, 100, 200, 200};
+        SDL_RenderFillRect(renderer, &rect);
+
+    }
 }
