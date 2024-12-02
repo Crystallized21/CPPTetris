95c47c9cbc446453779ed27586fde9f25eb6f756
Michael Bui <michael.bui_crystal@outlook.com>
2024-12-02 23:08:16 +1300

    tetrominos

--- src/block.h
+++ src/block.h
@@ -9,15 +9,20 @@
 #include <vector>
 #include <map>
 #include "position.h"
+#include "colors.h"
 
 class Block {
 public:
     Block();
+
+    void Draw();
+
     int id;
     std::map<int, std::vector<Position>> cells;
 private:
     int cellSize;
-    int roationState;
+    int rotationState;
+    std::vector<Color> colors;
 };
 
 #endif //BLOCK_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <raylib.h>
 #include "grid.h"
+#include "blocks.cpp"
 
 int main() {
     Color darkBlue = {44, 44, 127, 255};
@@ -7,15 +8,15 @@ int main() {
     SetTargetFPS(120);
 
     Grid grid = Grid();
-    grid.grid[0][0] = 1;
-    grid.grid[3][5] = 4;
-    grid.grid[17][8] = 7;
     grid.print();
+
+    LBlock block = LBlock();
 
     while (WindowShouldClose() == false) {
         BeginDrawing();
         ClearBackground(darkBlue);
         grid.Draw();
+        block.Draw();
 
         EndDrawing();
     }
